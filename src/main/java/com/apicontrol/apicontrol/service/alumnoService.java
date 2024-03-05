99e00100b72b29eea7923e04655f4ab3b4d1d6fa
Nefta11 <neftaliarturohernandez@gmail.com>
2024-03-05 11:39:32 -0600

    Updated

--- src/main/java/com/apicontrol/apicontrol/service/alumnoService.java
+++ src/main/java/com/apicontrol/apicontrol/service/alumnoService.java
@@ -1,5 +1,12 @@
 package com.apicontrol.apicontrol.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class alumnoService {
-    
+    @Autowired
+    private
+
 }
+
