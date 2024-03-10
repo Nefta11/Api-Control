8f36e5208518e87fff8675cf718e1317708314da
Nefta11 <neftaliarturohernandez@gmail.com>
2024-03-10 12:50:00 -0600

    Updated

--- src/main/java/com/apicontrol/apicontrol/controller/MainController.java
+++ src/main/java/com/apicontrol/apicontrol/controller/MainController.java
@@ -5,12 +5,19 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.apicontrol.apicontrol.entity.Administrativo;
 import com.apicontrol.apicontrol.entity.Alumno;
 import com.apicontrol.apicontrol.entity.Docente;
 import com.apicontrol.apicontrol.service.AdministrativoService;
@@ -60,5 +67,46 @@ public class MainController {
         }
 
         return docentesPartial;
+    }
+
+    @GetMapping("/Administrativos")
+    public List<Map<String, Object>> getAllAdministrativosPartial() {
+        List<Administrativo> administrativos = administrativoService.getAllAdministrativos();
+        List<Map<String, Object>> administrativosPartial = new ArrayList<>();
+
+        for (Administrativo administrativo : administrativos) {
+            Map<String, Object> administrativoPartial = new LinkedHashMap<>();
+            administrativoPartial.put("administrativoID", administrativo.getAdministrativoID());
+            administrativoPartial.put("no_contro", administrativo.getNo_contro());
+            administrativoPartial.put("nombre", administrativo.getNombre());
+            administrativoPartial.put("apellidoPaterno", administrativo.getApellidoPaterno());
+            administrativoPartial.put("apellidoMaterno", administrativo.getApellidoMaterno());
+            administrativosPartial.add(administrativoPartial);
+        }
+
+        return administrativosPartial;
+    }
+
+        // Método para actualizar un administrativo
+    @PutMapping("/Administrativos/{administrativoID}")
+    public ResponseEntity<String> updateAdministrativo(@PathVariable Long administrativoID, @RequestBody Map<String, Object> administrativoUpdates) {
+        Optional<Administrativo> optionalAdministrativo = administrativoService.getAdministrativo(administrativoID);
+
+        if (optionalAdministrativo.isPresent()) {
+            Administrativo administrativo = optionalAdministrativo.get();
+
+            // Actualizar solo los campos permitidos
+            administrativo.setNo_contro((Integer) administrativoUpdates.get("no_contro"));
+            administrativo.setNombre((String) administrativoUpdates.get("nombre"));
+            administrativo.setApellidoPaterno((String) administrativoUpdates.get("apellidoPaterno"));
+            administrativo.setApellidoMaterno((String) administrativoUpdates.get("apellidoMaterno"));
+
+            // Guardar el administrativo actualizado
+            administrativoService.updateAdministrativo(administrativoID, administrativo);
+
+            return new ResponseEntity<>("Administrativo actualizado exitosamente", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Administrativo no encontrado", HttpStatus.NOT_FOUND);
+        }
     }
 }
